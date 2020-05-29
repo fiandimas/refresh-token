f8ed52ada0e146a396892caa0e11ff61974cafd5
fiandimas <kidaliez@gmail.com>
2020-05-29 08:36:01 +0700

    create: migration for user table

---
+++ database/migrations/2020_05_29_012918_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name', 255);
+            $table->string('username', 50);
+            $table->char('password', 60);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
