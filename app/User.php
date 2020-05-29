8974fdce30584f41bbec893aa5caad5c5a96fdf1
fiandimas <kidaliez@gmail.com>
2020-05-29 08:52:48 +0700

    create: user and user refresh token model

--- app/User.php
+++ app/User.php
@@ -2,31 +2,23 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model
 {
-    use Authenticatable, Authorizable;
+    use SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email',
+        'name', 'username', 'password'
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password',
+        'password', 'created_at', 'updated_at', 'deleted_at'
     ];
+
+    public function refreshToken()
+    {
+        return $this->hasOne(UserRefreshToken::class, 'user_id');
+    }
 }
---
+++ app/UserRefreshToken.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserRefreshToken extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'user_refresh_token';
+    protected $fillable = [
+        'name_id', 'token', 'expired_at'
+    ];
+
+    protected $hidden = [
+        'expired_at', 'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
