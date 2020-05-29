6b5a6cf9c37fb9e607be10d2781a4520b92fad8e
fiandimas <kidaliez@gmail.com>
2020-05-29 08:39:36 +0700

    create: migration for user refresh token table

---
+++ database/migrations/2020_05_29_013720_create_user_refresh_token_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserRefreshTokenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_refresh_token', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->unsignedTinyInteger('user_id');
+            $table->char('refresh_token', 64);
+            $table->dateTime('expired_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_refresh_token');
+    }
+}
